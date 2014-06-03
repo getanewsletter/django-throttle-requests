1e23bd2e02dc170f5180d16ecc7357d4a767c949
Andreas Karlsson <akarl818@users.noreply.github.com>
2014-06-03 16:08:10 +0200

    Create setup.py

---
+++ throttle/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+import os
+
+setup(
+    name='throttle',
+    packages=find_packages(),
+    install_requires=[],
+    include_package_data=True,
+)
